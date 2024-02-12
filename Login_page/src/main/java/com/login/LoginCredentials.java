ebf0f989ff9051afe1c43f2545e67e38b2213ac6
Ashik <Ashik@Shahil>
2024-02-12 20:17:54 +0530

    The login page is created by using GenericServlet and it was the first commit

---
+++ Login_page/src/main/java/com/login/LoginCredentials.java
@@ -0,0 +1,60 @@
+package com.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/login")
+public class LoginCredentials extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		String user = req.getParameter("user");
+		String password = req.getParameter("password");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/college", "root", "root");
+			Statement st = con.createStatement();
+//			PreparedStatement ps = con.prepareStatement("select password from studentlogin where user = ?");
+//			ps.executeQuery();
+			try {
+				ResultSet rs = st.executeQuery("select password from studentlogin where user = '" + user + "'");
+				rs.next();
+				String p = rs.getString(1);
+				if (password.equals(p)) {
+					RequestDispatcher rd = req.getRequestDispatcher("afterLogin.html");
+					rd.forward(req, res);
+				} else {
+					PrintWriter pout = res.getWriter();
+					pout.println("Enter valid credentials");
+					RequestDispatcher rd = req.getRequestDispatcher("login.html");
+					rd.include(req, res);
+					res.setContentType("text/html");
+				}
+			} catch (SQLException e) {
+				PrintWriter pout = res.getWriter();
+				pout.println("Enter valid credentials");
+				RequestDispatcher rd = req.getRequestDispatcher("login.html");
+				rd.include(req, res);
+				res.setContentType("text/html");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
